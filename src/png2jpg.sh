a2a72cb96ffe6567156f9660bae8a49c7efa085d
lijun695 <lijun695@jd.com>
2024-04-28 20:34:41 +0800

    增加新命令

---
+++ src/png2jpg.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# WebP to PNG
+#
+
+basedir=`pwd`
+echo "basedir=${basedir}"
+
+for  f in `ls $1/*.png`
+do
+  if [ ! -f "$f" ]; then
+    continue
+  fi
+
+  # https://stackoverflow.com/a/965072/951836
+  fullname=$(basename -- "$f")
+  extension="${fullname##*.}"
+  filename="${fullname%.*}"
+  jpgname="${filename}.jpg"
+
+  # https://linuxize.com/post/bash-check-if-file-exists/
+  if [ -f "$jpgname" ]; then
+    echo "$jpgname exists, skip..."
+    continue
+  fi
+
+  echo "convert $fullname to $jpgname"
+  docker run -v ${basedir}:/imgs dpokidov/imagemagick /imgs/${f} /imgs/${jpgname}
+
+  echo ""
+done
+
